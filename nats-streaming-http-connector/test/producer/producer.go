9e4f2da91e90dbfd7d6a747bf14112519d45c029
Suramya <shah.suramya@gmail.com>
2021-06-16 18:41:49 +0530

    fix docker connection refused error in gh actions

---
+++ nats-streaming-http-connector/test/producer/producer.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"strconv"
+
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/stan.go"
+)
+
+func main() {
+	nc, err := nats.Connect("nats://localhost:4222")
+	if err != nil {
+		log.Fatal(err)
+	}
+	sc, err := stan.Connect("test-cluster", "stan-sub", stan.NatsConn(nc))
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i := 100; i < 200; i++ {
+		sc.Publish("request", []byte("Test"+strconv.Itoa(i)))
+	}
+
+	select {}
+}
