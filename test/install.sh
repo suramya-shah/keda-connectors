4734fc745d6339ab18f12774c16b48ad71160a16
Suramya <shah.suramya@gmail.com>
2021-06-16 20:49:56 +0530

    fix go version issue on nats

--- test/install.sh
+++ test/install.sh
@@ -10,4 +10,5 @@ git clone --single-branch --branch master https://github.com/fission/fission.git
 cd fission/charts/fission-core 
 helm dependency update . 
 helm install fission-app . --set mqt_keda.enabled=true --set mqt_keda.connector_images.nats_steaming.image=localhost:5000/nats-steaming --set mqt_keda.connector_images.nats_steaming.tag=latest --set mqt_keda.connector_images.awssqs.image=localhost:5000/aws-sqs-test-connector --set mqt_keda.connector_images.awssqs.tag=latest --set mqt_keda.connector_images.kafka.image='localhost:5000/kafka-connector' --set mqt_keda.connector_images.kafka.tag=latest --set mqt_keda.connector_images.rabbitmq.image=localhost:5000/rabbit-keda --set mqt_keda.connector_images.rabbitmq.tag=latest -n fission
-curl -Lo fission https://github.com/fission/fission/releases/download/1.13.0/fission-1.13.0-darwin-amd64 && chmod +x fission && sudo mv fission /usr/local/bin/
+curl -Lo fission https://github.com/fission/fission/releases/download/1.13.0/fission-1.13.0-linux-amd64 \
+    && chmod +x fission && sudo mv fission /usr/local/bin/
