9128c63282d0ad00787cbf157ef42328c53c6176
Suramya Shah <21042045+suramya-shah@users.noreply.github.com>
2021-06-23 20:44:45 +0530

    Update producer.go

--- nats-streaming-http-connector/test/producer/producer.go
+++ nats-streaming-http-connector/test/producer/producer.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	nc, err := nats.Connect("nats://defaultFissionAuthToken@nats-streaming.fission:4222")
+	nc, err := nats.Connect("nats-streaming.fission:4222")
 	if err != nil {
 		log.Fatal(err)
 	}
