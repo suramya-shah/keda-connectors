1fb7ae28cfd4dc749fb4441241ca850dd12f999d
Suramya Shah <21042045+suramya-shah@users.noreply.github.com>
2021-06-23 19:51:41 +0530

    Update producer.go

--- nats-streaming-http-connector/test/producer/producer.go
+++ nats-streaming-http-connector/test/producer/producer.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	nc, err := nats.Connect("nats-streaming.fission.svc.local:4222")
+	nc, err := nats.Connect("nats://nats-streaming.fission.svc.local:4222")
 	if err != nil {
 		log.Fatal(err)
 	}
