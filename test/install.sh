e51f8b4722edd797ebffa7d2e109a07c711367f7
Suramya <shah.suramya@gmail.com>
2021-06-24 13:14:23 +0530

    commit changes

--- test/install.sh
+++ test/install.sh
@@ -16,7 +16,7 @@ helm repo update
 helm install  --version 1.13.0 --namespace $FISSION_NAMESPACE  fission fission-charts/fission-core
 
 helm dependency update . 
-helm install fission-app . --set mqt_keda.enabled=true --set mqt_keda.connector_images.nats_steaming.image=localhost:5000/nats-steaming --set mqt_keda.connector_images.nats_steaming.tag=latest --set mqt_keda.connector_images.awssqs.image=localhost:5000/aws-sqs-test-connector --set mqt_keda.connector_images.awssqs.tag=latest --set mqt_keda.connector_images.kafka.image='localhost:5000/kafka-connector' --set mqt_keda.connector_images.kafka.tag=latest --set mqt_keda.connector_images.rabbitmq.image=localhost:5000/rabbit-keda --set mqt_keda.connector_images.rabbitmq.tag=latest -n fission
+helm install fission-app . --set mqt_keda.enabled=true --set mqt_keda.connector_images.nats_steaming.image=localhost:5000/nats-steaming --set mqt_keda.connector_images.nats_steaming.tag=latest --set mqt_keda.connector_images.awssqs.image=localhost:5000/aws-sqs-test-connector --set mqt_keda.connector_images.awssqs.tag=latest --set mqt_keda.connector_images.kafka.image=localhost:5000/kafka-connector --set mqt_keda.connector_images.kafka.tag=latest --set mqt_keda.connector_images.rabbitmq.image=localhost:5000/rabbit-keda --set mqt_keda.connector_images.rabbitmq.tag=latest -n fission
 
 # install fission cli
 curl -Lo fission https://github.com/fission/fission/releases/download/1.13.0/fission-1.13.0-linux-amd64 
