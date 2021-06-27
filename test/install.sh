9f530a3fc41fa9ee2ed5668bf2e431265750b3a1
Suramya <shah.suramya@gmail.com>
2021-06-27 18:42:11 +0530

    commit changes

--- test/install.sh
+++ test/install.sh
@@ -13,15 +13,8 @@ kubectl create namespace $FISSION_NAMESPACE
 kubectl create -k "github.com/fission/fission/crds/v1?ref=1.13.0"
 helm repo add fission-charts https://fission.github.io/fission-charts/
 helm repo update
-<<<<<<< HEAD
-helm install  --version 1.13.0 --namespace $FISSION_NAMESPACE  fission fission-charts/fission-core
-
-helm dependency update . 
-helm install fission-app . --set mqt_keda.enabled=true --set mqt_keda.connector_images.nats_steaming.image=localhost:5000/nats-steaming --set mqt_keda.connector_images.nats_steaming.tag=latest --set mqt_keda.connector_images.awssqs.image=localhost:5000/aws-sqs-test-connector --set mqt_keda.connector_images.awssqs.tag=latest --set mqt_keda.connector_images.kafka.image=localhost:5000/kafka-connector --set mqt_keda.connector_images.kafka.tag=latest --set mqt_keda.connector_images.rabbitmq.image=localhost:5000/rabbit-keda --set mqt_keda.connector_images.rabbitmq.tag=latest -n fission
-=======
 #helm install  --version 1.13.0 --namespace $FISSION_NAMESPACE  fission fission-charts/fission-core
 helm install  --version 1.13.0 --namespace $FISSION_NAMESPACE  fission fission-charts/fission-all --set mqt_keda.enabled=true --set mqt_keda.connector_images.nats_steaming.image=localhost:5000/nats-steaming --set mqt_keda.connector_images.nats_steaming.tag=latest --set mqt_keda.connector_images.awssqs.image=localhost:5000/aws-sqs-test-connector --set mqt_keda.connector_images.awssqs.tag=latest --set mqt_keda.connector_images.kafka.image='localhost:5000/kafka-connector' --set mqt_keda.connector_images.kafka.tag=latest --set mqt_keda.connector_images.rabbitmq.image=localhost:5000/rabbit-keda --set mqt_keda.connector_images.rabbitmq.tag=latest -n fission
->>>>>>> e653f60d0f6886cfe79cd941d921c9106d944a16
 
 # install fission cli
 curl -Lo fission https://github.com/fission/fission/releases/download/1.13.0/fission-1.13.0-linux-amd64 
