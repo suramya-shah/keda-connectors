a94a2c864907fdad8978c4d6e17c09b848fb6048
Suramya Shah <21042045+suramya-shah@users.noreply.github.com>
2021-06-28 11:39:08 +0530

    Update producer.go

--- aws-sqs-http-connector/test/producer/producer.go
+++ aws-sqs-http-connector/test/producer/producer.go
@@ -1,18 +1,16 @@
 package main
-
 import (
 	"fmt"
 	"log"
-
+	"net/http"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
 )
-
-func main() {
+func Handler(w http.ResponseWriter, r *http.Request) {
 	queueURL := "http://localstack.default.svc.cluster.local:31000/queue/my_queue"
-	region := "us-east-1"
+	region := "us-east-2"
 	config := &aws.Config{
 		Region:      &region,
 		Credentials: credentials.NewStaticCredentials("foo", "bar", ""),
@@ -22,15 +20,17 @@ func main() {
 		log.Panic("Error while creating session")
 	}
 	svc := sqs.New(sess)
-	for i := 100; i < 110; i++ {
-		msg := fmt.Sprintf("Hello Msg %v", i+1)
+	for i := 1; i <= 10; i++ {
+		msg := fmt.Sprintf("message count %v", i+1)
 		_, err := svc.SendMessage(&sqs.SendMessageInput{
 			DelaySeconds: aws.Int64(10),
 			MessageBody:  &msg,
 			QueueUrl:     &queueURL,
 		})
 		if err != nil {
-			log.Panic("Error while writing message")
+			w.Write([]byte(fmt.Sprintf("failed to send message to input queue: %v", err)))
+			return
 		}
 	}
+	w.Write([]byte("successfully sent message to input queue"))
 }
